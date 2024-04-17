cdf346bcf78a519c8d69ae4bd7e52a3cc8c1e07a
swarajpande5 <swarajpande5@gmail.com>
2024-04-17 16:02:48 +0530

    Improve __main__.py by adding menu

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,27 @@
 from src.rag import create_index_pdf, generate_response
 
-pdf_path = input("Enter path of the pdf: ")
 
-collection = create_index_pdf(pdf_path)
+def print_options():
+    print("1. Add pdf to knowledgebase")
+    print("2. Ask a query")
+    print("3. Exit")
+    option = input("Enter your option: ")
+    
+    return option
 
-query = input("Enter query: ")
 
-response = generate_response(query)
-
-print(response)
+if __name__ == '__main__':
+    while(True):
+        op = print_options()
+        if op == '1':
+            pdf_path = input("Enter path of the pdf: ")
+            collection = create_index_pdf(pdf_path)
+            print("\nAdded the pdf to the knowledgebase.\n")
+        elif op == '2':
+            query = input("Enter query: ")
+            response = generate_response(query)
+            print(f"{response} \n")
+        elif op == '3':
+            break 
+        else:
+            print("Invalid Option. \n")
