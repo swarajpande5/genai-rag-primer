1979812a69914d86bef61c6dcb7607ccd48699f6
swarajpande5 <swarajpande5@gmail.com>
2024-04-18 11:56:41 +0530

    Add changes to display the embedding vector

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,13 @@
+from src.chroma import Chroma
+from src.embedder import GeminiEmbeddingFunction
 from src.rag import create_index_pdf, generate_response
 
 
 def print_options():
     print("1. Add pdf to knowledgebase")
-    print("2. Ask a query")
-    print("3. Exit")
+    print("2. Print embeddings in the vector db")
+    print("3. Ask a query")
+    print("4. Exit")
     option = input("Enter your option: ")
     
     return option
@@ -18,10 +21,13 @@ if __name__ == '__main__':
             collection = create_index_pdf(pdf_path)
             print("\nAdded the pdf to the knowledgebase.\n")
         elif op == '2':
+            chroma_instance = Chroma(embedding_function=GeminiEmbeddingFunction())
+            print(f"\n{chroma_instance._collection.get(include=['embeddings'])}\n")
+        elif op == '3':
             query = input("Enter query: ")
             response = generate_response(query)
             print(f"{response} \n")
-        elif op == '3':
+        elif op == '4':
             break 
         else:
-            print("Invalid Option. \n")
+            print("\nInvalid Option.\n")
--- src/utils.py
+++ src/utils.py
@@ -2,5 +2,5 @@ import re
 
 
 def split_text(text: str):
-    split_text = re.split('\n \n', text)
+    split_text = re.split('\n\n', text)
     return [d for d in split_text if d != ""]
